e6ee8b63531e335f4c33461dbcaf08141fb38983
Lee Hyeon Ju <sinbihae@gmail.com>
2021-02-18 09:36:51 +0900

    update

--- http-server.go
+++ http-server.go
@@ -8,7 +8,7 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"google.golang.org/grpc"
 
-	userpb "github.com/sinbihae/go-grpc-example/protos/v2/user"
+	userpb "github.com/sinbihae/go-grpc-gateway-example/protos/v2/user"
 )
 
 const (
